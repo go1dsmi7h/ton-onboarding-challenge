a19b3ca6f86f50edc898f906f41c3b7c5071caac
go1dsmi7h <seriga333@gmail.com>
2024-04-29 18:34:18 +0300

    first commit

--- index.ts
+++ index.ts
@@ -1,2 +1,25 @@
+import {Address, TonClient} from "ton"
+import {getHttpEndpoint} from "@orbs-network/ton-access";
 
-// write your NFT miner here
+async function main () {
+
+  const wallet = Address.parse('0QCJ_2a9meTE66ykvSW-Agz6-RF8HXdxsiwqMrYUtmy_i73P');
+  const collection = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');
+
+  // get the decentralized RPC endpoint in Testnet
+const endpoint = await getHttpEndpoint({
+    network: "testnet",
+  });
+
+  // initialize ton library
+  const client = new TonClient({ endpoint });
+
+  const miningData = await client.callGetMethod(collection, 'get_mining_data')
+
+  console.log(miningData)
+
+}
+
+
+
+main()
